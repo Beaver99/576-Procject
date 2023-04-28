487bc03e6e799bf9486413592cd653a9f1af5c25
hfjl <hfjl1999@outlook.com>
2023-04-27 19:05:53 -0700

    20230427

--- src/IndexExtractor.java
+++ src/IndexExtractor.java
@@ -1,30 +1,35 @@
 import java.io.File;
+import java.util.ArrayList;
 
 public class IndexExtractor {
-    public static Index[] mockExtractIndex(File rgbs, File audio) {
+    public static ArrayList<Index> mockExtractIndex(File rgbs, File audio) {
         // for testing
-        return new Index[]{new Index(0, Level.scene),
-                new Index(0, Level.scene),
-                new Index(60, Level.scene),
-                new Index(120, Level.shot),
-                new Index(250, Level.shot),
-                new Index(400, Level.scene),
-                new Index(500, Level.shot),
-                new Index(600, Level.subshot),
-                new Index(700, Level.subshot),
-                new Index(1000, Level.shot),
-                new Index(10000, Level.scene),
-                new Index(11000, Level.shot),
-                new Index(11110, Level.shot),
-                new Index(11199, Level.subshot)
-        };
+        ArrayList<Index> idxs = new ArrayList<>();
+        idxs.add(new Index(0, Level.scene));
+
+        idxs.add(new Index(100, Level.scene));
+        idxs.add(new Index(200, Level.shot));
+        idxs.add(new Index(250, Level.shot));
+        idxs.add(new Index(300, Level.subshot));
+
+        idxs.add(new Index(1000, Level.scene));
+        idxs.add(new Index(2000, Level.shot));
+        idxs.add(new Index(3000, Level.subshot));
+        idxs.add(new Index(4000, Level.shot));
+        idxs.add(new Index(4500, Level.shot));
+
+        idxs.add(new Index(4800, Level.scene));
+
+        idxs.add(new Index(5000, Level.scene));
+        idxs.add(new Index(5300, Level.shot));
+        idxs.add(new Index(5315, Level.subshot));
+        idxs.add(new Index(5330, Level.subshot));
+        return idxs;
     }
 
-    public static Index[] extractIndex(File rgbs, File audio) {
+    public static ArrayList<Index> extractIndex(File rgbs, File audio) {
         // just a demo
-        return new Index[]{new Index(0, Level.scene)};
+        ArrayList<Index> idxs = new ArrayList<>();
+        return idxs;
     }
-
-    // get the histogram-difference array for any 2 consecutive frames.
-//    public static long[] histogramDifference(File rgbs, )
 }
