dea9ce32c5519e30da572822fdf61c2201983b19
hfjl <hfjl1999@outlook.com>
2023-04-27 18:54:07 -0700

    20230427 template not finished yet

--- src/IndexExtractor.java
+++ src/IndexExtractor.java
@@ -1,4 +1,9 @@
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class IndexExtractor {
     public static Index[] mockExtractIndex(File rgbs, File audio) {
@@ -21,10 +26,72 @@ public class IndexExtractor {
     }
 
     public static Index[] extractIndex(File rgbs, File audio) {
+        Index[] res = new Index[]{};
+        int width = 480; // width of the video frames
+        int height = 270; // height of the video frames
+        int fps = 30; // frames per second of the video
+        long numFrames = rgbs.length();
+
+        RandomAccessFile raf = null;
+        FileChannel channel = null;
+        ByteBuffer buffer = null;
+        try {
+            raf = new RandomAccessFile(rgbs, "r");
+            channel = raf.getChannel();
+            buffer = ByteBuffer.allocate(width * height * 3);
+            int[][] prevFrame = new int[width * height][];
+            int[][] currFrame = new int[width * height][];
+
+            // step 2
+            for (int i = 0; i < numFrames; i++) {
+                buffer.clear();
+                channel.read(buffer);
+                buffer.rewind();
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        int r = buffer.get() & 0xff;
+                        int g = buffer.get() & 0xff;
+                        int b = buffer.get() & 0xff;
+
+                        // use luv color space instead of rgb color space
+                        currFrame[y * width + x] = rgb2luv(r, g, b);
+
+                        if (i != 0) {
+
+                        } else {
+                            
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (channel != null) {
+                    channel.close();
+                }
+                if (raf != null) {
+                    raf.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         // just a demo
         return new Index[]{new Index(0, Level.scene)};
     }
 
+    // #todo
     // get the histogram-difference array for any 2 consecutive frames.
-//    public static long[] histogramDifference(File rgbs, )
+    public static long histogramDifference() {
+        return 1;
+    }
+
+    //#todo
+    // rgb to luv color space
+    public static int[] rgb2luv(int r, int g, int b) {
+        return new int[]{0, 0, 0};
+    }
+
 }
