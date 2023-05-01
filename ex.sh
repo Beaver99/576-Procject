6691ccbc5e33499e7d59e0aa4d7d4d18942720db
Jasmine Sodhi <sodhij@usc.edu>
2023-05-01 19:06:39 -0400

    Added compilation error handling in sh script

--- ex.sh
+++ ex.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd src
 javac -d ../out Level.java Index.java IndexExtractor.java Player.java Main.java
+if [ $? -eq 0 ]; then
 cd ..
 java -classpath ./out Main ./data/InputVideo.rgb ./data/InputAudio.wav
+else
+echo -e "\nCompilation failed"
+fi
