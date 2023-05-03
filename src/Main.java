1721fd811ad3cdf26365b373e898392f1913cfac
hfjl <hfjl1999@outlook.com>
2023-05-03 11:37:35 -0700

    230503 runnable now

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         File rgbs = new File(args[0]); // name of the RGB video file
         File audio = new File(args[1]); // name of the audio file
 
-//        Player.play(rgbs, audio, IndexExtractor.extractIndex(rgbs, audio));
-        Player.play(rgbs, audio, IndexExtractor.mockExtractIndex(rgbs, audio));
+        Player.play(rgbs, audio, IndexExtractor.extractIndex(rgbs, audio));
+//        Player.play(rgbs, audio, IndexExtractor.mockExtractIndex(rgbs, audio));
     }
 }
